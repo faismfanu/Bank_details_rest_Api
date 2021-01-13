da4435c626a707184f2611d05efdb35fb765be82
FAIS MANGALASSERI <67838155+faismfanu@users.noreply.github.com>
2021-01-13 22:19:03 +0530

    Update urls.py

--- Bankdatas/bank_info/urls.py
+++ Bankdatas/bank_info/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views 
   
 urlpatterns = [ 
-    path('api/<str:ifsc>/', views.Bank_ifsc.as_view(), name ='Bank Ifsc'), 
-    path('api/<str:bank_name>/<str:city>/', views.Bank_branch.as_view(), name ='Bank Ifsc'), 
+    path('api/<str:ifsc>/', views.BankIfsc.as_view(), name ='Bank Ifsc'), 
+    path('api/<str:bank_name>/<str:city>/', views.BankBranch.as_view(), name ='Bank Branch'), 
 
 
 ] 
