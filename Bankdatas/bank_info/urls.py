d48d3d57768c3de3ec508d3762b6e5f2d4ab3c22
FAIS MANGALASSERI <67838155+faismfanu@users.noreply.github.com>
2021-01-14 12:01:40 +0530

    Update urls.py

--- Bankdatas/bank_info/urls.py
+++ Bankdatas/bank_info/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [ 
     path('api/<str:ifsc>/', views.BankIfsc.as_view(), name ='Bank Ifsc'), 
     path('api/<str:bank_name>/<str:city>/', views.BankBranch.as_view(), name ='Bank Branch'), 
+    path('api/',include('rest_framework.urls', namespace = 'rest_framework'))
 
 
 ] 
